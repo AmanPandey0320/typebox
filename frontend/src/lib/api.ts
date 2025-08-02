c8781971f15738036d239d455459a8218ef995d8
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2025-08-02 21:45:46 +0530

    added fetch client

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,10 +1,29 @@
-export const getDirname = (dirId: string) => {
-    // TODO: Implement logic to get the directory name based on dirId
-    return dirId;
-};
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
 
+export interface Response {
+    error: any[],
+    data: any[],
+    message: any[]
+}
+
+export interface ApiErrorResponse {
+    timestamp: string;
+    status: number;
+    error: string;
+    path: string;
+}
 export const getFilesInDirectory = async (dirId: string) => {
     // TODO: Implement logic to get the files in the directory based on dirId
+    const response = await fetch(`${BASE_URL}/file/define/${dirId}`);
+
+    if (response.ok) {
+        const data: Response = await response.json();
+        return data;
+    } else {
+        //handle error
+        const errorData: ApiErrorResponse | Response = await response.json();
+    }
+
     return [
         { name: "File1.tsx", size: 1234 },
         { name: "File2.tsx", size: 5678 },
